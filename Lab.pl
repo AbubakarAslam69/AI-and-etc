47c962ad483bb738831a4a761f7d87736699ad2b
Abubakar Aslam <127650749+Akagi69@users.noreply.github.com>
2023-03-12 00:16:40 -0800

    Create Lab file work.

---
+++ Lab.pl
@@ -0,0 +1,6 @@
+man(umair).
+man(amir).
+man(jaree).
+man(abubakar).
+female(aelia).
+female(nabia).
---
+++ lab1.pl
@@ -0,0 +1,8 @@
+father(arslan,saima).
+friend(ali,farhan).
+drive(farhan,car).
+likes(ali,icecream).
+woman(saima).
+man(farhan).
+walk(farhan).
+happy(ali).
